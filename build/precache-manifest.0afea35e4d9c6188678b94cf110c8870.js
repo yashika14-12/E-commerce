5bd6d6a76ec67dfde5cd6ba5bbee4be3e0cf253d
Yashika Sharma <95169271+yashika14-12@users.noreply.github.com>
2023-03-22 22:44:31 +0530

    Add files via upload

---
+++ build/precache-manifest.0afea35e4d9c6188678b94cf110c8870.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249264ddd651801fa1c1dd651ee72839",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f7a1e4975435859f570",
+    "url": "/static/js/2.7b6a6afc.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.7b6a6afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34262abe44b6be4930de",
+    "url": "/static/js/main.8a873908.chunk.js"
+  },
+  {
+    "revision": "e49b3101035638cf7d71",
+    "url": "/static/js/runtime-main.6a6a2054.js"
+  }
+]);
